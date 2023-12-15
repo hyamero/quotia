924e37c4f828f3c3dc6cc61d19184abe0f1fd99d
Dale B <daleban.dev@gmail.com>
2023-12-15 08:32:17 +0800

    initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  driver: "mysql2",
+  dbCredentials: {
+    connectionString: env.DATABASE_URL,
+  },
+  tablesFilter: ["quotia_*"],
+} satisfies Config;
