b98de3bd35249e58ea91069701c27968a1259169
Dale B <daleban.dev@gmail.com>
2024-01-06 17:39:41 +0800

    refactor: clean up posts router syntax

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,13 +1,14 @@
 import { z } from "zod";
 import { nanoid } from "nanoid";
+import { eq, and, desc } from "drizzle-orm";
 
 import {
   createTRPCRouter,
   protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
+
 import { likes, posts } from "~/server/db/schema";
-import { eq, and, desc } from "drizzle-orm";
 
 export const postRouter = createTRPCRouter({
   create: protectedProcedure
@@ -19,20 +20,16 @@ export const postRouter = createTRPCRouter({
         id: nanoid(),
       });
 
-      const data = await ctx.db
-        .select({
-          id: posts.id,
-          authorId: posts.authorId,
-          content: posts.content,
-          createdAt: posts.createdAt,
-        })
-        .from(posts)
-        .where(eq(posts.authorId, ctx.session.user.id))
-        .orderBy(desc(posts.createdAt))
-        .limit(1)
-        .execute();
-
-      return data[0];
+      return await ctx.db.query.posts.findFirst({
+        where: eq(posts.authorId, ctx.session.user.id),
+        orderBy: [desc(posts.createdAt)],
+        columns: {
+          id: true,
+          authorId: true,
+          content: true,
+          createdAt: true,
+        },
+      });
     }),
 
   delete: protectedProcedure
@@ -43,7 +40,7 @@ export const postRouter = createTRPCRouter({
 
   getAll: publicProcedure.query(async ({ ctx }) => {
     const data = await ctx.db.query.posts.findMany({
-      orderBy: (posts, { desc }) => [desc(posts.createdAt)],
+      orderBy: [desc(posts.createdAt)],
       with: {
         author: true,
         likes: true,
@@ -67,11 +64,10 @@ export const postRouter = createTRPCRouter({
     .input(z.object({ postId: z.string() }))
     .mutation(async ({ ctx, input }) => {
       const existingLike = await ctx.db.query.likes.findFirst({
-        where: (likes, { and, eq }) =>
-          and(
-            eq(likes.postId, input.postId),
-            eq(likes.userId, ctx.session.user.id),
-          ),
+        where: and(
+          eq(likes.postId, input.postId),
+          eq(likes.userId, ctx.session.user.id),
+        ),
       });
 
       if (existingLike == undefined || existingLike == null) {
