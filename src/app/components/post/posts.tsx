cade9f8baf42c7a6ba99446031fa7e70358c58e5
Dale B <daleban.dev@gmail.com>
2024-01-07 22:20:17 +0800

    feat: infinite feed with cursor based pagination

--- src/app/components/post/posts.tsx
+++ src/app/components/post/posts.tsx
@@ -4,26 +4,43 @@ import { useEffect } from "react";
 import { type Session } from "next-auth";
 
 import {
-  type Post,
   type User,
   useTempPosts,
   useSetSession,
   useDeletedPosts,
 } from "~/lib/useStore";
 
+import { api } from "~/trpc/react";
 import { PostItem } from "./post-item";
 import { CreatePost } from "./create-post";
 import { DeletePostModal } from "../modals";
+import { useInView } from "react-intersection-observer";
+import React from "react";
+import Loading from "~/app/loading";
 
 type PostsProps = {
   session?: Session | null;
-  allPosts: Post[] | null;
 };
 
-export function Posts({ session, allPosts }: PostsProps) {
+export function Posts({ session }: PostsProps) {
+  const { ref, inView } = useInView();
+
+  const {
+    data: posts,
+    isLoading,
+    // isFetchingNextPage,
+    fetchNextPage,
+    // isError,
+    // error,
+  } = api.post.inifiniteFeed.useInfiniteQuery(
+    {},
+    {
+      getNextPageParam: (lastPage) => lastPage.nextPageCursor ?? undefined,
+    },
+  );
+
   const tempPosts = useTempPosts();
   const setSession = useSetSession();
-
   const deletedPosts = useDeletedPosts();
 
   useEffect(() => {
@@ -37,6 +54,14 @@ export function Posts({ session, allPosts }: PostsProps) {
     if (tempPostId) return true;
     else return false;
   };
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage().catch((err) => console.log(err));
+    }
+  }, [fetchNextPage, inView]);
+
+  if (isLoading) return <Loading />;
 
   return (
     <div className="mt-24 w-full max-w-lg xl:max-w-xl">
@@ -60,18 +85,20 @@ export function Posts({ session, allPosts }: PostsProps) {
             })
         : null}
 
-      {allPosts?.length !== 0 ? (
-        allPosts
-          ?.filter(
-            (post) =>
-              !deletedPosts.includes(post.id) && !existsInTempPosts(post.id),
-          )
-          .map((post) => {
-            return <PostItem key={post.id} post={post} />;
-          })
-      ) : (
-        <p className="text-3xl font-bold text-white">No posts yet.</p>
-      )}
+      {posts?.pages.map((page) => (
+        <React.Fragment key={page.nextPageCursor?.toDateString()}>
+          {page.posts
+            .filter(
+              (post) =>
+                !deletedPosts.includes(post.id) && !existsInTempPosts(post.id),
+            )
+            .map((post) => (
+              <PostItem key={post.id} post={post} />
+            ))}
+        </React.Fragment>
+      ))}
+
+      <div ref={ref} />
     </div>
   );
 }
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { nanoid } from "nanoid";
-import { eq, and, desc } from "drizzle-orm";
+import { eq, and, desc, gt, is, lte } from "drizzle-orm";
 
 import {
   createTRPCRouter,
@@ -11,6 +11,48 @@ import {
 import { likes, posts } from "~/server/db/schema";
 
 export const postRouter = createTRPCRouter({
+  inifiniteFeed: publicProcedure
+    .input(
+      z.object({
+        limit: z.number().optional().default(7),
+        cursor: z.date().optional(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const data = await ctx.db.query.posts.findMany({
+        orderBy: [desc(posts.createdAt)],
+        limit: input.limit + 1,
+        where: lte(posts.createdAt, input.cursor ?? new Date()),
+        with: {
+          author: true,
+          likes: true,
+        },
+      });
+
+      let nextPageCursor: typeof input.cursor | undefined;
+
+      if (data.length > input.limit) {
+        const nextItem = data[data.length - 1];
+
+        if (nextItem !== null && nextItem !== undefined) {
+          nextPageCursor = nextItem.createdAt;
+        }
+      }
+
+      return {
+        posts: data.map((post) => {
+          return {
+            ...post,
+            likes: post.likes.length,
+            likedByUser: post.likes.some(
+              (like) => like.userId === ctx.session?.user.id,
+            ),
+          };
+        }),
+        nextPageCursor,
+      };
+    }),
+
   create: protectedProcedure
     .input(z.object({ content: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
@@ -37,28 +79,6 @@ export const postRouter = createTRPCRouter({
     .mutation(async ({ ctx, input }) => {
       await ctx.db.delete(posts).where(eq(posts.id, input.postId));
     }),
-
-  getAll: publicProcedure.query(async ({ ctx }) => {
-    const data = await ctx.db.query.posts.findMany({
-      orderBy: [desc(posts.createdAt)],
-      with: {
-        author: true,
-        likes: true,
-      },
-    });
-
-    return {
-      posts: data.map((post) => {
-        return {
-          ...post,
-          likes: post.likes.length,
-          likedByUser: post.likes.some(
-            (like) => like.userId === ctx.session?.user.id,
-          ),
-        };
-      }),
-    };
-  }),
 
   toggleLike: protectedProcedure
     .input(z.object({ postId: z.string() }))
