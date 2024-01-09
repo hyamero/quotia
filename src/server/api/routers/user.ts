86d8b17f74f41a150dc0e02538ab772ba9ad0a90
Dale B <daleban.dev@gmail.com>
2024-01-09 18:02:26 +0800

    chore(router): setup user with slug router

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -4,13 +4,33 @@ import {
   //   protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
+import { eq, or } from "drizzle-orm";
+import { users } from "~/server/db/schema";
 
 export const userRouter = createTRPCRouter({
   getUser: publicProcedure
     .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      return await ctx.db.query.users.findFirst({
+        where: or(eq(users.id, input.id), eq(users.slug, input.id)),
+        columns: {
+          id: true,
+          slug: true,
+          name: true,
+          email: true,
+          image: true,
+        },
+      });
+    }),
+
+  getUserSlug: publicProcedure
+    .input(z.object({ id: z.string() }))
     .query(({ ctx, input }) => {
       return ctx.db.query.users.findFirst({
         where: (users, { eq }) => eq(users.id, input.id),
+        columns: {
+          slug: true,
+        },
       });
     }),
 });
