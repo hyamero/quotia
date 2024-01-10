a15f8d4a41507a5f47c6d5d1f9a08b8f0ac329ce
Dale B <daleban.dev@gmail.com>
2024-01-10 14:49:42 +0800

    feat: add next dynamic metadata on profile page

--- src/app/user/[slug]/page.tsx
+++ src/app/user/[slug]/page.tsx
@@ -1,12 +1,43 @@
 import { api } from "~/trpc/server";
 import UserProfile from "../user-profile";
+import type { User } from "~/lib/useStore";
+
+const removeSlug = (param: string) => {
+  return param.startsWith("%40") ? param.split("%40").at(1) : param;
+};
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const userId = removeSlug(params.slug);
+
+  const user = (await api.user.getUser.query({
+    id: userId!,
+    columns: {
+      slug: true,
+      name: true,
+    },
+  })) as User;
+
+  return {
+    title: `${user.name} (@${user.slug}) on Quotia`,
+  };
+}
 
 export default async function Page({ params }: { params: { slug: string } }) {
-  const userId = params.slug.startsWith("%40")
-    ? params.slug.split("%40").at(1)
-    : params.slug;
+  const userId = removeSlug(params.slug);
 
-  const user = await api.user.getUser.query({ id: userId! });
+  const user = (await api.user.getUser.query({
+    id: userId!,
+    columns: {
+      id: true,
+      slug: true,
+      name: true,
+      image: true,
+    },
+  })) as User;
 
   return <UserProfile user={user} />;
 }
