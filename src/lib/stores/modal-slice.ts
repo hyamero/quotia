86b02d9dc70c573b8fea1a03edd59f33a4456a63
Dale B <daleban.dev@gmail.com>
2024-01-18 23:05:04 +0800

    chore: add comment form modal states in zustand store

--- src/lib/stores/modal-slice.ts
+++ src/lib/stores/modal-slice.ts
@@ -4,10 +4,16 @@ export type ModalSlice = {
   postFormIsOpen: boolean;
   loginModalIsOpen: boolean;
   deletePostModalIsOpen: boolean;
+  commentFormIsOpen: {
+    isOpen: boolean;
+    postId: string;
+  };
 
   modalActions: {
     setPostFormIsOpen: (modalState: boolean) => void;
+    setCommentFormIsOpen: (modalState: boolean) => void;
     togglePostFormIsOpen: () => void;
+    toggleCommentFormIsOpen: (postId: string) => void;
     toggleLoginModalIsOpen: () => void;
     toggleDeletePostModalIsOpen: () => void;
   };
@@ -17,6 +23,10 @@ export const createModalSlice: StateCreator<ModalSlice> = (set) => ({
   postFormIsOpen: false,
   loginModalIsOpen: false,
   deletePostModalIsOpen: false,
+  commentFormIsOpen: {
+    isOpen: false,
+    postId: "",
+  },
 
   modalActions: {
     setPostFormIsOpen: (modalState) =>
@@ -24,8 +34,24 @@ export const createModalSlice: StateCreator<ModalSlice> = (set) => ({
         postFormIsOpen: modalState,
       })),
 
+    setCommentFormIsOpen: (modalState) =>
+      set(() => ({
+        commentFormIsOpen: {
+          isOpen: modalState,
+          postId: "",
+        },
+      })),
+
     togglePostFormIsOpen: () =>
       set((state) => ({ postFormIsOpen: !state.postFormIsOpen })),
+
+    toggleCommentFormIsOpen: (postId) =>
+      set((state) => ({
+        commentFormIsOpen: {
+          isOpen: !state.commentFormIsOpen.isOpen,
+          postId: postId,
+        },
+      })),
 
     toggleLoginModalIsOpen: () =>
       set((state) => ({ loginModalIsOpen: !state.loginModalIsOpen })),
