be991b228d2d2a9e500609e3f5413edbb45786dd
Dale B <daleban.dev@gmail.com>
2024-01-26 18:22:45 +0800

    feat: view likes list

--- src/app/_components/post/post-item.tsx
+++ src/app/_components/post/post-item.tsx
@@ -6,7 +6,7 @@ import { api } from "~/trpc/react";
 import { toast } from "sonner";
 import Link from "next/link";
 
-import type { Post, User } from "~/lib/types";
+import type { Like, Post, User } from "~/lib/types";
 import { PiChatCircle, PiHeart, PiHeartFill } from "react-icons/pi";
 import { formatDistance } from "~/lib/format-distance";
 import { formatDistanceToNowStrict, formatRelative } from "date-fns";
@@ -29,6 +29,7 @@ import {
 } from "../ui/hover-card";
 import { Button } from "../ui/button";
 import { useBoundStore } from "~/lib/use-bound-store";
+import { Dialog, DialogContent } from "../ui/dialog";
 
 type PostItemProps = {
   post: Post;
@@ -43,6 +44,7 @@ export function PostItem({ post, postType = "post" }: PostItemProps) {
   const toggleCommentFormIsOpen = useBoundStore(
     (state) => state.modalActions.toggleCommentFormIsOpen,
   );
+
   const [likedByUser, setLikedByUser] = useState(post.likedByUser);
   const [likeCount, setLikeCount] = useState(post.likes ? post.likes : 0);
   const [likes, setLikes] = useState(
@@ -55,6 +57,8 @@ export function PostItem({ post, postType = "post" }: PostItemProps) {
       setLikedByUser(!likedByUser);
     },
   });
+
+  const { data: postLikes } = api.post.viewLikes.useQuery({ postId: post.id });
 
   const handleToggleLikeCount = () => {
     if (!user) {
@@ -81,122 +85,164 @@ export function PostItem({ post, postType = "post" }: PostItemProps) {
 
   const userSlug = post.author.slug ? "@" + post.author.slug : post.authorId;
 
+  const [likesModalIsOpen, setLikesModalIsOpen] = useState(false);
+  likesModalIsOpen;
+
   return (
-    <div className="flex items-start justify-between border-b py-5 text-[#f2f4f6]">
-      <div className="flex w-full items-start gap-3">
-        <Link href={`/user/${userSlug}`} className="font-semibold">
-          <Avatar className="relative top-1">
-            <AvatarImage
-              className="rounded-full"
-              src={post.author.image as string | undefined}
-              alt={`${post.author.name}'s avatar`}
-            />
-            <AvatarFallback className="text-xs">
-              {post.author.name?.split(" ").at(0)}
-            </AvatarFallback>
-          </Avatar>
-        </Link>
+    <>
+      <ViewLikes
+        postLikes={postLikes}
+        likesModalIsOpen={likesModalIsOpen}
+        setLikesModalIsOpen={setLikesModalIsOpen}
+      />
+      <div className="flex items-start justify-between border-b py-5 text-[#f2f4f6]">
+        <div className="flex w-full items-start gap-3">
+          <Link href={`/user/${userSlug}`} className="font-semibold">
+            <Avatar className="relative top-1">
+              <AvatarImage
+                className="rounded-full"
+                src={post.author.image as string | undefined}
+                alt={`${post.author.name}'s avatar`}
+              />
+              <AvatarFallback className="text-xs">
+                {post.author.name?.split(" ").at(0)}
+              </AvatarFallback>
+            </Avatar>
+          </Link>
 
-        <div className="w-full">
-          <div className="flex justify-between">
-            <HoverCardProfile author={post.author} userId={user?.id}>
-              <Link
-                href={`/user/${userSlug}`}
-                className="font-semibold hover:underline"
-              >
-                {post.author.name}
-              </Link>
-            </HoverCardProfile>
+          <div className="w-full">
+            <div className="flex justify-between">
+              <HoverCardProfile author={post.author} userId={user?.id}>
+                <Link
+                  href={`/user/${userSlug}`}
+                  className="font-semibold hover:underline"
+                >
+                  {post.author.name}
+                </Link>
+              </HoverCardProfile>
 
-            <div className="flex items-center gap-2">
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <span className="select-none text-zinc-500">
-                      {formatDistanceToNowStrict(post.createdAt, {
-                        addSuffix: false,
-                        locale: {
-                          formatDistance: (...props) =>
-                            formatDistance(...props),
-                        },
-                      })}
-                    </span>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <span>{formatRelative(post.createdAt, new Date())}</span>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-              {/* 
+              <div className="flex items-center gap-2">
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <span className="select-none text-zinc-500">
+                        {formatDistanceToNowStrict(post.createdAt, {
+                          addSuffix: false,
+                          locale: {
+                            formatDistance: (...props) =>
+                              formatDistance(...props),
+                          },
+                        })}
+                      </span>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <span>{formatRelative(post.createdAt, new Date())}</span>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+                {/* 
                DropDownMenu
               */}
-              {postType === "post" && (
-                <PostDropdownMenu postId={post.id} postAuthor={post.authorId} />
-              )}
-            </div>
-          </div>
-
-          {!pathname.includes("/post") && postType === "post" ? (
-            <Link
-              href={`/user/${userSlug}/post/${post.id}`}
-              className="whitespace-pre-wrap"
-            >
-              {post.content}
-            </Link>
-          ) : (
-            <p className="whitespace-pre-wrap">{post.content}</p>
-          )}
-
-          {postType === "post" && (
-            <>
-              <div className="relative right-[0.4rem] mt-[6px] text-[#e6e8ea]">
-                <button
-                  title="like"
-                  type="button"
-                  className="rounded-full p-[0.4rem] transition-colors duration-200 hover:bg-zinc-900"
-                  onClick={() => {
-                    handleToggleLikeCount();
-                  }}
-                >
-                  {likedByUser ? (
-                    <PiHeartFill className="transform text-2xl text-red-500 transition-transform active:scale-90" />
-                  ) : (
-                    <PiHeart className="transform text-2xl transition-transform active:scale-90" />
-                  )}
-                </button>
-
-                <button
-                  title="comment"
-                  type="button"
-                  className="rounded-full p-[0.4rem] transition-colors duration-200 hover:bg-zinc-900"
-                  onClick={() => toggleCommentFormIsOpen(post)}
-                >
-                  <PiChatCircle className="text-2xl" />
-                </button>
-              </div>
-
-              <div className="space-x-3">
-                {!pathname.includes(post.id) && (
-                  <Link
-                    className="text-zinc-500"
-                    href={`/user/${post.author.slug ?? post.authorId}/post/${
-                      post.id
-                    }`}
-                  >
-                    replies
-                  </Link>
+                {postType === "post" && (
+                  <PostDropdownMenu
+                    postId={post.id}
+                    postAuthor={post.authorId}
+                  />
                 )}
-                <span className="text-zinc-500">
-                  {(likeCount ? likeCount : "") + " " + likes}
-                </span>
               </div>
-            </>
-          )}
+            </div>
+
+            {!pathname.includes("/post") && postType === "post" ? (
+              <Link
+                href={`/user/${userSlug}/post/${post.id}`}
+                className="whitespace-pre-wrap"
+              >
+                {post.content}
+              </Link>
+            ) : (
+              <p className="whitespace-pre-wrap">{post.content}</p>
+            )}
+
+            {postType === "post" && (
+              <>
+                <div className="relative right-[0.4rem] mt-[6px] text-[#e6e8ea]">
+                  <button
+                    title="like"
+                    type="button"
+                    className="rounded-full p-[0.4rem] transition-colors duration-200 hover:bg-zinc-900"
+                    onClick={() => {
+                      handleToggleLikeCount();
+                    }}
+                  >
+                    {likedByUser ? (
+                      <PiHeartFill className="transform text-2xl text-red-500 transition-transform active:scale-90" />
+                    ) : (
+                      <PiHeart className="transform text-2xl transition-transform active:scale-90" />
+                    )}
+                  </button>
+
+                  <button
+                    title="comment"
+                    type="button"
+                    className="rounded-full p-[0.4rem] transition-colors duration-200 hover:bg-zinc-900"
+                    onClick={() => toggleCommentFormIsOpen(post)}
+                  >
+                    <PiChatCircle className="text-2xl" />
+                  </button>
+                </div>
+
+                <div className="space-x-3">
+                  {!pathname.includes(post.id) && (
+                    <Link
+                      className="text-zinc-500"
+                      href={`/user/${post.author.slug ?? post.authorId}/post/${
+                        post.id
+                      }`}
+                    >
+                      replies
+                    </Link>
+                  )}
+                  <span
+                    className="cursor-pointer text-zinc-500"
+                    onClick={() => setLikesModalIsOpen(!likesModalIsOpen)}
+                  >
+                    {(likeCount ? likeCount : "") + " " + likes}
+                  </span>
+                </div>
+              </>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
+
+type ViewLikesProps = {
+  postLikes: Like[] | undefined;
+  likesModalIsOpen: boolean;
+  setLikesModalIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const ViewLikes = ({
+  postLikes,
+  likesModalIsOpen,
+  setLikesModalIsOpen,
+}: ViewLikesProps) => {
+  return (
+    <Dialog open={likesModalIsOpen} onOpenChange={setLikesModalIsOpen}>
+      <DialogContent>
+        <ul>
+          {postLikes?.map((like) => (
+            <li key={like.postId + like.userId}>
+              {like.user.slug ? "@" + like.user.slug : like.user.name}
+            </li>
+          ))}
+        </ul>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 type HoverCardProfileProps = {
   children: React.ReactNode;
