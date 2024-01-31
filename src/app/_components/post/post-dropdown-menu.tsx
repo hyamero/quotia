998844f743efe5b29a8204286039ba8fb06e9b71
Dale B <daleban.dev@gmail.com>
2024-01-31 22:34:21 +0800

    refactor: map post menu items

--- src/app/_components/post/post-dropdown-menu.tsx
+++ src/app/_components/post/post-dropdown-menu.tsx
@@ -11,6 +11,7 @@ import {
   DropdownMenuTrigger,
 } from "~/app/_components/ui/dropdown-menu";
 import { useBoundStore } from "~/lib/use-bound-store";
+import React from "react";
 
 type PostDropdownMenuProps = {
   postId: string;
@@ -35,6 +36,67 @@ export function PostDropdownMenu({
     toggleDeletePostModalIsOpen();
   };
 
+  type PostMenu = {
+    title: string;
+    onClick?: () => void;
+    className?: string;
+  };
+
+  const authorMenu: PostMenu[] = [
+    {
+      title: "Pin to profile",
+      onClick: () => {
+        toast.error("Not implemented yet");
+      },
+    },
+    {
+      title: "Who can reply",
+      onClick: () => {
+        toast.error("Not implemented yet");
+      },
+    },
+    {
+      title: "Hide count",
+      onClick: () => {
+        toast.error("Not implemented yet");
+      },
+    },
+    {
+      title: "Delete",
+      onClick: handleDeletePost,
+      className: "text-red-500",
+    },
+  ];
+
+  const userMenu: PostMenu[] = [
+    {
+      title: "Profile",
+      onClick: () => {
+        toast.error("Not implemented yet");
+      },
+    },
+    {
+      title: "Hide",
+      onClick: () => {
+        toast.error("Not implemented yet");
+      },
+    },
+    {
+      title: "Block",
+      onClick: () => {
+        toast.error("Not implemented yet");
+      },
+      className: "text-red-500",
+    },
+    {
+      title: "Report",
+      onClick: () => {
+        toast.error("Not implemented yet");
+      },
+      className: "text-red-500",
+    },
+  ];
+
   return (
     <>
       {isAuthor ? (
@@ -43,25 +105,18 @@ export function PostDropdownMenu({
             <PiDotsThree className="text-2xl" />
           </DropdownMenuTrigger>
           <DropdownMenuContent className="font-semibold [&>*]:cursor-pointer">
-            <button
-              type="button"
-              onClick={() => {
-                toast.error("Not implemented yet");
-              }}
-            >
-              <DropdownMenuItem>Pin to profile</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Who can reply</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Hide count</DropdownMenuItem>
-            </button>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              onClick={handleDeletePost}
-              className=" text-red-500"
-            >
-              Delete
-            </DropdownMenuItem>
+            {authorMenu.map((item, i) => (
+              <>
+                <DropdownMenuItem
+                  key={item.title}
+                  onClick={item.onClick}
+                  className={item.className}
+                >
+                  {item.title}
+                </DropdownMenuItem>
+                {i + 1 !== authorMenu.length && <DropdownMenuSeparator />}
+              </>
+            ))}
           </DropdownMenuContent>
         </DropdownMenu>
       ) : (
@@ -73,15 +128,19 @@ export function PostDropdownMenu({
             onClick={() => {
               toast.error("Not implemented yet");
             }}
-            className="font-semibold [&>*]:cursor-pointer"
+            className="font-semibold [&>*]:cursor-pointer [&>*]:border-b [&>*]:last:border-0"
           >
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Hide</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="text-red-500">Block</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="text-red-500">Report</DropdownMenuItem>
+            {userMenu.map((item, i) => (
+              <React.Fragment key={item.title}>
+                <DropdownMenuItem
+                  onClick={item.onClick}
+                  className={item.className}
+                >
+                  {item.title}
+                </DropdownMenuItem>
+                {i + 1 !== userMenu.length && <DropdownMenuSeparator />}
+              </React.Fragment>
+            ))}
           </DropdownMenuContent>
         </DropdownMenu>
       )}
